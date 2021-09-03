6704921b2d2233ff20b8e5396ef3364d95641794
wuyue112524 <you@example.com>
2021-09-03 20:54:44 +0100

    add EWaste model framework

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     miner_revenue_url = os.getenv('miner_revenue_url')
     network_hashrate_url = os.getenv('network_hashrate_url')
     mining_equipment_url = os.getenv('mining_equipment_url')
+    electricity_cost = float(os.getenv('electricity_cost'))
     
     data_loader = DataCollect()
     miner_revenue = data_loader.load_quandl_api(miner_revenue_url,['date','miner_rev'])
--- src/EWaste.py
+++ src/EWaste.py
@@ -1,2 +1,24 @@
 class EWaste:
-    pass
+    def __init__(self,miner_revenue,network_hashrate,mining_equipment):
+        '''
+        param:
+            miner_revenue: dataframe
+            network_hashrate: dataframe
+            mining_equipment: dataframe
+
+        attribute:
+            miner_cost: dataframe
+            miner_profitability: dataframe
+        '''
+        self.miner_revenue = miner_revenue
+        self.network_hashrate = network_hashrate
+        self.mining_equipment = mining_equipment
+        self.miner_cost = None
+        self.miner_profitability = None
+        
+    def calculate_daily_cost(self,electricity_cost):
+        pass
+        
+    
+    def calculate_daily_profitability(self):
+        pass
